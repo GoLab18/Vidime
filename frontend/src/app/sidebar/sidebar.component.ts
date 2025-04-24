255d083d79d5b3e958fc93af0a0a5e27d4659345
GoLab18 <kuba1golab@gmail.com>
2025-04-24 10:48:59 +0200

    Added a collapsing sidebar component

---
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [RouterLink],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  @Input() collapsed = false;
+}
