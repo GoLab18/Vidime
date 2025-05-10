2d1ba507762089464d845cfa2e90a0323df40950
Jakub Gołąb <kuba1golab@gmail.com>
2025-05-10 22:56:43 +0200

    Added an initial channel page

---
+++ frontend/src/app/pages/channel/channel.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Channel } from '../../models/channel.model';
+import { ChannelService } from '../../services/channel.service';
+
+@Component({
+  selector: 'app-channel',
+  imports: [CommonModule],
+  templateUrl: './channel.component.html',
+  styleUrl: './channel.component.css'
+})
+export class ChannelComponent implements OnInit {
+  channel!: Channel;
+
+  constructor(private channelService: ChannelService) {}
+
+  ngOnInit(): void {
+    this.channelService.getChannel(101).subscribe(channel => this.channel = channel);
+  }
+}
