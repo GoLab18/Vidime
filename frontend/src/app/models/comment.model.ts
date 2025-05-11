41c7fa2be6d7f12faca7bff2e6d176b7b2773a73
Jakub Gołąb <kuba1golab@gmail.com>
2025-05-12 00:27:30 +0200

    Angular comment model adjusted

--- frontend/src/app/models/comment.model.ts
+++ frontend/src/app/models/comment.model.ts
@@ -4,17 +4,18 @@ import { Video } from './video.model';
 export interface Comment {
   id: number;
   channel: Channel;
-  video: Video;
+  video?: Video;
   content: string;
   likes: number;
   dislikes: number;
+  repliesAmount: number;
   createdAt: string;
 }
 
 export interface Reply {
   id: number;
   channel: Channel;
-  parentComment: Comment;
+  parentComment?: Comment;
   likes: number;
   dislikes: number;
   content: string;
