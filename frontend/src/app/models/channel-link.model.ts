8c0eff0571a1de13efd4b0fd8d4a3bc9d0398478
Jakub Gołąb <kuba1golab@gmail.com>
2025-05-12 22:00:00 +0200

    Implemented a side channel section with links for the channel page

---
+++ frontend/src/app/models/channel-link.model.ts
@@ -0,0 +1,7 @@
+export interface ChannelLink {
+  id: number;
+  channelId: number;
+  title: string;
+  url: string;
+  position: number;
+}
--- frontend/src/app/pages/channel/channel.component.ts
+++ frontend/src/app/pages/channel/channel.component.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 import { Channel } from '../../models/channel.model';
 import { ChannelService } from '../../services/channel.service';
 import { FormatNumberPipe } from '../../pipes/format-number.pipe';
+import { FormatDatePipe } from '../../pipes/format-date.pipe';
 import { ActivatedRoute } from '@angular/router';
+import { ChannelLink } from '../../models/channel-link.model';
 
 @Component({
   selector: 'app-channel',
-  imports: [CommonModule, FormatNumberPipe],
+  imports: [CommonModule, FormatNumberPipe, FormatDatePipe],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.css'
 })
 export class ChannelComponent implements OnInit {
   @Input({required: true}) videoId!: number;
+
   channel!: Channel;
+  links!: ChannelLink[];
 
   constructor(private channelService: ChannelService, private route: ActivatedRoute) {}
 
@@ -21,7 +25,10 @@ export class ChannelComponent implements OnInit {
     this.route.queryParamMap.subscribe((params) => {
       this.videoId = Number(params.get('i'));
       this.videoId = 101; // TODO only for testing
-      this.channelService.getChannel(this.videoId).subscribe(channel => this.channel = channel);
+      this.channelService.getChannel(this.videoId).subscribe(channel => {
+        this.channel = channel;
+        this.channelService.getLinks(this.channel.id).subscribe(links => this.links = links);
+      });
     });
   }
 }
