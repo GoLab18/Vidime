0396907a2a792ff5fccabeca0b1d751633c10997
Jakub Gołąb <kuba1golab@gmail.com>
2025-06-12 20:49:59 +0200

    Added a dropdown menu for logged in users and sign in button for logged out ones

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,19 +1,37 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
   imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   searchQuery: string = '';
+  isLoggedIn = false;
+  private authSubscription: Subscription | undefined;
   
   @Output() sidebarToggle = new EventEmitter<void>();
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.authSubscription = this.authService.currUserId$.subscribe(userId => {
+      this.isLoggedIn = !!userId;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.authSubscription) this.authSubscription.unsubscribe();
+  }
 
   toggleSidebar() {
     this.sidebarToggle.emit();
@@ -21,5 +39,13 @@ export class HeaderComponent {
 
   onSearch() {
     console.log('Searching for:', this.searchQuery);
+  }
+
+  onLogin() {
+    this.router.navigate(['/auth']);
+  }
+
+  onLogout() {
+    this.authService.manualLogout();
   }
 }
