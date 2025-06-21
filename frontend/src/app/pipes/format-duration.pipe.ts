31cda0f43978eaee2f0ba86e6714d9d10e88e219
Jakub Gołąb <kuba1golab@gmail.com>
2025-06-22 01:17:48 +0200

    Made the pipe for duration format work in millis

--- frontend/src/app/pipes/format-duration.pipe.ts
+++ frontend/src/app/pipes/format-duration.pipe.ts
@@ -5,12 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatDurationPipe implements PipeTransform {
   transform(value: number): string {
-    let hours = Math.floor(value / 3600);
-    let minutes = Math.floor((value % 3600) / 60);
-    let seconds = value % 60;
+    const secondsFromMillis = Math.floor(value / 1000);
+    const hours = Math.floor(secondsFromMillis / 3600);
+    const minutes = Math.floor((secondsFromMillis % 3600) / 60);
+    const seconds = secondsFromMillis % 60;
 
-    let minutesStr = minutes.toString().padStart(hours > 0 ? 2 : 1, '0');
-    let secondsStr = seconds.toString().padStart(2, '0');
+    const minutesStr = minutes.toString().padStart(hours > 0 ? 2 : 1, '0');
+    const secondsStr = seconds.toString().padStart(2, '0');
 
     return hours > 0
       ? `${hours}:${minutesStr}:${secondsStr}`
