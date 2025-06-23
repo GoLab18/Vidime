039d171aca58b97518c2113611568f711e4493f0
Jakub Gołąb <kuba1golab@gmail.com>
2025-06-23 18:47:59 +0200

    Added basic view for trending channels and videos

--- frontend/src/app/pages/trending/trending.component.ts
+++ frontend/src/app/pages/trending/trending.component.ts
@@ -2,23 +2,55 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoSlim } from '../../models/video.model';
 import { VideoService } from '../../services/video.service';
-import { HomeVideoComponent } from '../../components/home-video/home-video.component';
+import { HorizontalGroupScrollComponent } from '../../components/horizontal-group-scroll/horizontal-group-scroll.component';
+import { ChannelTrending } from '../../models/channel.model';
+import { ChannelService } from '../../services/channel.service';
+import { HorizontalChannelListComponent } from '../../components/horizontal-channel-list/horizontal-channel-list.component';
 
 @Component({
   selector: 'app-trending',
-  standalone: true,
-  imports: [CommonModule, HomeVideoComponent],
+  imports: [CommonModule, HorizontalGroupScrollComponent, HorizontalChannelListComponent],
   templateUrl: './trending.component.html',
   styleUrl: './trending.component.css'
 })
 export class TrendingComponent implements OnInit {
-  videos: VideoSlim[] = [];
+  mostSubsChannelsAllTime: ChannelTrending[] = [];
+  mostViewedChannelsAllTime: ChannelTrending[] = [];
+  mostViewedChannelsLastWeek: ChannelTrending[] = [];
+  mostViewedVideosAllTime: VideoSlim[] = [];
+  mostViewedVideosLastWeek: VideoSlim[] = [];
+  bestRatedVideosAllTime: VideoSlim[] = [];
+  bestRatedVideosLastWeek: VideoSlim[] = [];
 
-  constructor(private videoService: VideoService) { }
+  constructor(private videoService: VideoService, private channelService: ChannelService) {}
 
-  ngOnInit(): void {
-    this.videoService.getTrendingVideos().subscribe(data => {
-      this.videos = data;
+  ngOnInit() {
+    this.channelService.getMostSubsChannelsAllTime().subscribe(data => {
+      this.mostSubsChannelsAllTime = data;
+    });
+
+    this.channelService.getMostViewedChannelsAllTime().subscribe(data => {
+      this.mostViewedChannelsAllTime = data;
+    });
+
+    this.channelService.getMostViewedChannelsLastWeek().subscribe(data => {
+      this.mostViewedChannelsLastWeek = data;
+    });
+
+    this.videoService.getMostViewedVideosAllTime().subscribe(data => {
+      this.mostViewedVideosAllTime = data;
+    });
+
+    this.videoService.getMostWatchedVideosLastWeek().subscribe(data => {
+      this.mostViewedVideosLastWeek = data;
+    });
+
+    this.videoService.getBestRatedVideosAllTime().subscribe(data => {
+      this.bestRatedVideosAllTime = data;
+    });
+
+    this.videoService.getBestRatedVideosLastWeek().subscribe(data => {
+      this.bestRatedVideosLastWeek = data;
     });
   }
 }
