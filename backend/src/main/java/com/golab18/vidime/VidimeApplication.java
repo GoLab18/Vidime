3e055eeb985a53af390bbd6126ec7030a9aadd0b
Jakub Gołąb <kuba1golab@gmail.com>
2025-06-23 18:56:06 +0200

    Enabled scheduling in application root.

--- backend/src/main/java/com/golab18/vidime/VidimeApplication.java
+++ backend/src/main/java/com/golab18/vidime/VidimeApplication.java
@@ -2,8 +2,10 @@ package com.golab18.vidime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VidimeApplication {
 
 	public static void main(String[] args) {
