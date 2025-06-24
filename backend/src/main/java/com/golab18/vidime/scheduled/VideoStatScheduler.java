cf61f3145e0f96963056a9f624737a6e8e787008
Jakub Gołąb <kuba1golab@gmail.com>
2025-06-24 13:15:23 +0200

    Replaced older scheduler class with VideoStatScheduler

---
+++ backend/src/main/java/com/golab18/vidime/scheduled/VideoStatScheduler.java
@@ -0,0 +1,30 @@
+package com.golab18.vidime.scheduled;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.golab18.vidime.service.VideoService;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class VideoStatScheduler {
+    
+    private final VideoService videoService;
+
+    @Scheduled(cron = "0 0 * * * ?")
+    public void periodicBayesianRatingUpdate() {
+        videoService.updateBayesianRatings();
+    }
+
+    @Scheduled(cron = "0 0 * * * ?")
+    public void periodicTimeDecayedBayesianRatingUpdate() {
+        videoService.updateTimeDecayedBayesianRatings();
+    }
+
+    @Scheduled(cron = "0 0 * * * ?")
+    public void periodicDecayedViewsUpdate() {
+        videoService.updateDecayedViews();
+    }
+}
