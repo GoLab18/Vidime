898df2bb7f9eaed8df31bdf3bdfa10bb9cd6458f
Jakub Gołąb <kuba1golab@gmail.com>
2025-06-24 13:14:48 +0200

    Finished horizontal-channel-list component, added lazy loading for channels

--- frontend/src/app/components/horizontal-channel-list/horizontal-channel-list.component.ts
+++ frontend/src/app/components/horizontal-channel-list/horizontal-channel-list.component.ts
@@ -1,12 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { ChannelTrending } from '../../models/channel.model';
+import { Router } from '@angular/router';
+import { FormatNumberPipe } from '../../pipes/format-number.pipe';
 
 @Component({
   selector: 'app-horizontal-channel-list',
-  imports: [],
+  imports: [FormatNumberPipe],
   templateUrl: './horizontal-channel-list.component.html',
   styleUrl: './horizontal-channel-list.component.css'
 })
 export class HorizontalChannelListComponent {
   @Input() channels: ChannelTrending[] = [];
+
+  constructor(private router: Router) {}
+
+  navigateToChannel(channelId: number, channelUuid: string) {
+    this.router.navigate(['/channel'], { queryParams: { i: channelId, c: channelUuid } });
+  }
 }
