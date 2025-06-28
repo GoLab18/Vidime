e2c9aefd21af396462c15ca9f6b86b7542d46d1c
Jakub Gołąb <kuba1golab@gmail.com>
2025-06-28 15:43:50 +0200

    Created a video history list container

--- frontend/src/app/pages/history/history.component.ts
+++ frontend/src/app/pages/history/history.component.ts
@@ -3,10 +3,11 @@ import { WatchHistoryService } from '../../services/watch-history.service';
 import { StrippedPage } from '../../util/paging';
 import { HistoryVideo } from '../../models/video.model';
 import { HistoryVideoComponent } from '../../components/history-video/history-video.component';
+import { FormatWatchHistoryDatePipe } from '../../pipes/format-watch-history-date.pipe';
 
 @Component({
   selector: 'app-history',
-  imports: [HistoryVideoComponent],
+  imports: [HistoryVideoComponent, FormatWatchHistoryDatePipe],
   templateUrl: './history.component.html',
   styleUrl: './history.component.css'
 })
@@ -23,5 +24,26 @@ export class HistoryComponent implements OnInit {
     this.watchHistoryService.fetchWatchHistoryVideosBatch(before, pageNumber, pageSize).subscribe((data) => {
       this.paginatedVideos = data;
     });
+  }
+
+  trackByVideoKey(_: number, videoId: number, lastWatchedAt: string): string {
+    return `${videoId}-${lastWatchedAt}`;
+  }
+
+  shouldShowDateHeader(videos: HistoryVideo[], index: number): boolean {
+    if (index === 0) return true;
+
+    const current = new Date(videos[index].lastWatchedAt);
+    const prev = new Date(videos[index - 1].lastWatchedAt);
+  
+    return !this.isSameDay(current, prev);
+  }
+  
+  isSameDay(a: Date, b: Date): boolean {
+    return (
+      a.getFullYear() === b.getFullYear() &&
+      a.getMonth() === b.getMonth() &&
+      a.getDate() === b.getDate()
+    );
   }
 }
