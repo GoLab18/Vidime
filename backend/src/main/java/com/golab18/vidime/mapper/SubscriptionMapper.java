869fb1d5d9869948820f5e14580bca97f2119066
Jakub Gołąb <kuba1golab@gmail.com>
2025-06-28 22:46:26 +0200

    Mapper for subs fix

--- backend/src/main/java/com/golab18/vidime/mapper/SubscriptionMapper.java
+++ backend/src/main/java/com/golab18/vidime/mapper/SubscriptionMapper.java
@@ -9,15 +9,15 @@ import com.golab18.vidime.entity.Subscription;
 import java.sql.Timestamp;
 import java.time.Instant;
 
-@Mapper(componentModel = "spring", uses = {ChannelMapper.class}, imports = {Timestamp.class, Instant.class})
+@Mapper(componentModel = "spring", imports = {Timestamp.class, Instant.class})
 public interface SubscriptionMapper {
     @Mapping(target = "subscriberId", source = "subscriber.id")
     @Mapping(target = "channelId", source = "channel.id")
     @Mapping(target = "subscribedAt", expression = "java(subscription.getSubscribedAt() != null ? subscription.getSubscribedAt().toString() : null)")
     SubscriptionDto toDto(Subscription subscription);
     
-    @Mapping(target = "subscriber", expression = "java(subscriptionDto.getSubscriberId() != null ? channelMapper.toEntity(subscriptionDto.getSubscriberId()) : null)")
-    @Mapping(target = "channel", expression = "java(subscriptionDto.getChannelId() != null ? channelMapper.toEntity(subscriptionDto.getChannelId()) : null)")
+    @Mapping(target = "subscriber", expression = "java(subscriptionDto.getSubscriberId() != null ? new Channel(subscriptionDto.getSubscriberId()) : null)")
+    @Mapping(target = "channel", expression = "java(subscriptionDto.getChannelId() != null ? new Channel(subscriptionDto.getChannelId()) : null)")
     @Mapping(target = "subscribedAt", expression = "java(subscriptionDto.getSubscribedAt() != null ? Timestamp.from(Instant.parse(subscriptionDto.getSubscribedAt())) : null)")
     Subscription toEntity(SubscriptionDto subscriptionDto);
 
