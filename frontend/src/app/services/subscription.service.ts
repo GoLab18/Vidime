86b9839abea2d3542c3448a83ef8647db5f41787
Jakub Gołąb <kuba1golab@gmail.com>
2025-06-29 14:35:06 +0200

    Added a subscription service method for fetching day-aggregated subscription statistics.

--- frontend/src/app/services/subscription.service.ts
+++ frontend/src/app/services/subscription.service.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { catchError, Observable, of, throwError } from 'rxjs';
 import { env } from '../../environments/env';
 import { Subscription } from '../models/subscription.model';
+import { DailyAggregation } from '../models/daily-aggregation.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +13,13 @@ export class SubscriptionService {
   private apiUrl = env.apiUrl + '/subscriptions';
   
   constructor(private http: HttpClient, private authService: AuthService) {}
-
+  
   // Returns observable Subscription object when subscription found, otherwise on 404 returns null.
   getCurrChannelSubscriptionTo(channelId: number): Observable<Subscription | null> {
     const params = new HttpParams()
-      .set('subscriberId', this.authService.currentChannelId!)
-      .set('channelId', channelId);
-  
+    .set('subscriberId', this.authService.currentChannelId!)
+    .set('channelId', channelId);
+    
     return this.http.get<Subscription>(`${this.apiUrl}/check`, { params }).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === 404) return of(null);
@@ -26,12 +27,36 @@ export class SubscriptionService {
       })
     );
   }
-
+  
   subscribeTo(channelId: number): Observable<Subscription> {
     return this.http.post<Subscription>(`${this.apiUrl}/sub`, { subscriberId: this.authService.currentChannelId!, channelId });
   }
-
+  
   unsubscribe(subscriptionId: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/unsub/${subscriptionId}`);
+  }
+
+  getSubscriptionsByChannelPerDay(channelId: number, timePeriod: 'last7Days' | 'last30Days' | 'last90Days'): Observable<DailyAggregation[]> {
+    const start = new Date(), end = new Date();
+    switch (timePeriod) {
+      case 'last7Days':
+        start.setDate(start.getDate() - 6);
+        break;
+      case 'last30Days':
+        start.setDate(start.getDate() - 29);
+        break;
+      case 'last90Days':
+        start.setDate(start.getDate() - 89);
+        break;
+    }
+
+    const params = new HttpParams()
+      .set('start', start.toISOString().split('T')[0])
+      .set('end', end.toISOString().split('T')[0]);
+    
+    return this.http.get<DailyAggregation[]>(
+      `${this.apiUrl}/count/${channelId}`,
+      { params }
+    );
   }
 }
